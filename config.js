ac8b8028f06c1e4e55ad7127f887a3d826dcb596
Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 💸 <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-11-23 18:37:03 +0530

    ϙᴜᴇᴇɴ-ᴅ ~ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ

---
+++ config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "", //your session id
+MV_SEND_JID: process.env.MV_SEND_JID || "120363197756141044@g.us", //default movie send group
+MODE: process.env.MODE || "public", //private or inbox or groups
+AUTO_AI: process.env.AUTO_AI || "off", //on or off
+AUTO_AI_JID: process.env.AUTO_AI_JID || "", //auto ai working group
+BLOCK_JID: process.env.BLOCK_JID || "", //block jids
+};
