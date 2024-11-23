39c9933f2edfb398e52baaf698f153de618666af
Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 💸 <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-11-23 18:53:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //your session id
+SESSION_ID: process.env.SESSION_ID || "DqgmHCCD#CJLpRU_zf8VyizKlNzLyISCTGMINKt-f5kUeOEN3AnI", //your session id
 MV_SEND_JID: process.env.MV_SEND_JID || "120363197756141044@g.us", //default movie send group
 MODE: process.env.MODE || "public", //private or inbox or groups
 AUTO_AI: process.env.AUTO_AI || "off", //on or off
